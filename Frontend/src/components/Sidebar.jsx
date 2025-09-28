e84959d485967b5a8e1797ad23be73ccb2296d2d
Pratik17SV <pratik.dg@somaiya.edu>
2025-09-29 00:08:32 +0530

    Friends Path created

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -14,6 +14,13 @@ import logo from '../assets/nav_icon.png';
           <path d="M11.47 3.84a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 1-1.06 1.06l-.4-.4V19.5A2.25 2.25 0 0 1 17.5 21.75h-2.25a.75.75 0 0 1-.75-.75v-3.75a1.5 1.5 0 0 0-1.5-1.5h-1.5a1.5 1.5 0 0 0-1.5 1.5V21a.75.75 0 0 1-.75.75H5.5A2.25 2.25 0 0 1 3.25 19.5v-6.31l-.4.4a.75.75 0 1 1-1.06-1.06l8.69-8.69Z"/>
         </svg>
       ) },
+      { to:'/friends', label:'Friends', icon:(
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-5 w-5">
+          <path d="M12 12c2.485 0 4.5-2.015 4.5-4.5S14.485 3 12 3 7.5 5.015 7.5 7.5 9.515 12 12 12z" />
+          <path fillRule="evenodd" d="M4.5 19.5a7.5 7.5 0 0 1 15 0v.75a.75.75 0 0 1-.75.75H5.25a.75.75 0 0 1-.75-.75v-.75z" clipRule="evenodd" />
+        </svg>
+      ) },
+      
       { to: '/notifications', label: 'Notifications', icon: (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-5 w-5">
           <path d="M14.25 18.75a2.25 2.25 0 1 1-4.5 0h4.5Z"/>
